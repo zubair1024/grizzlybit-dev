34fff9d6f1dc5fc99e6aeace023d33b2c1a0cda8
Zubair Ahmed <zubair1024@gmail.com>
2023-01-23 14:54:14 +0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['synthwave'],
+    darkTheme: 'dark',
+  },
+};
