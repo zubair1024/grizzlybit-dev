85eacf66fb98dc80736657bb4e116a1258dcbf6e
Zubair Ahmed <zubair1024@gmail.com>
2023-01-27 03:40:41 +0400

    adding razrlibre portfolio card

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -1,5 +1,15 @@
 const portfolio = [
   {
+    img: '/portfolio/razrtrack-mobile.jpg',
+    desc: 'A mobile application for remotely tracking telemetry data for vehicle and drivers',
+    title: 'RAZRTRACK Mobile',
+  },
+  {
+    img: '/portfolio/razrlibre.jpg',
+    desc: 'A remote health monitoring application for covid patients',
+    title: 'RAZRLIBRE Mobile',
+  },
+  {
     img: '/portfolio/my247.jpg',
     desc: 'An App for the 4th Larger Boiler Service Company in the UK',
     title: 'My247',
@@ -8,11 +18,6 @@ const portfolio = [
     img: '/portfolio/tis.jpg',
     desc: 'A website for the Thermal Insulation solution company in Dubai',
     title: 'TIS',
-  },
-  {
-    img: '/portfolio/razrtrack-mobile.jpg',
-    desc: 'A mobile application for remotely tracking telemetry data for vehicle and drivers',
-    title: 'RAZRTRACK Mobile',
   },
 ];
 
