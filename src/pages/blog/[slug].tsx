58447874fca9f7c38a0e11e27b92299f494f7af1
Zubair Ahmed <zubair1024@gmail.com>
2023-01-26 15:28:13 +0400

    porting in blog pages

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,12 +1,17 @@
+import Layout from '@/components/Layout';
 import { getMDXComponent } from 'mdx-bundler/client';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useMemo } from 'react';
 import { getAllPostSlugs, getPostData } from '../../util/posts';
 
 type Frontmatter = {
   title: string;
-  date: string;
+  publishedAt: string;
   description: string;
+  summary: string;
+  image: string;
 };
 
 type Props = {
@@ -17,23 +22,48 @@ type Props = {
 export default function BlogPost({ code, frontmatter }: Props) {
   const Component = useMemo(() => getMDXComponent(code), [code]);
 
+  console.log(frontmatter);
   return (
-    <article
-      itemScope
-      itemType="http://schema.org/Article"
-      className="prose prose-xl"
-    >
-      <header>
-        <h1 itemProp="headline">{frontmatter.title}</h1>
-        <p>{frontmatter.date}</p>
-      </header>
-      <section
-        itemProp="articleBody"
-        className="mx-auto my-4 prose md:prose-lg lg:prose-xl"
-      >
-        <Component />
-      </section>
-    </article>
+    <>
+      <Layout>
+        <div className="max-w-full prose prose-xl">
+          <div className="bg-fixed bg-toolBelt">
+            <div className="py-10 overflow-hidden text-white bg-black bg-opacity-50 shadow-lg toolBelt-content backdrop-filter backdrop-blur-lg">
+              <h2 className="p-16 text-center md:grid-cols-4">
+                {frontmatter.title}
+              </h2>
+            </div>
+          </div>
+          <div className="max-w-[900px] mx-auto py-10 px-10 bg-base-300">
+            <Component />
+            <div className="divider"></div>
+            <div className="flex items-center justify-center space-x-5">
+              <div>
+                <Image
+                  src="/zubair_2.jpg"
+                  height={200}
+                  width={200}
+                  alt="Zubair Ahmed"
+                  className="w-96 rounded-xl"
+                ></Image>
+              </div>
+              <div className="text-sm">
+                <p>Published on {frontmatter.publishedAt}</p>
+                <h2>Zubair Ahmed</h2>
+                <p>
+                  I&apos;m a developer, an entrepreneur, an ambitious tweaker,
+                  author, traveller and over-scrutinizer 😝 . I work at RAZRLAB
+                  as the Chief Technology Officer.
+                </p>
+                <Link href={'/'} className="no-underline">
+                  <button className="btn-sm btn btn-accent">Read More</button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </>
   );
 }
 
