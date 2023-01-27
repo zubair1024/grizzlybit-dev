b2577d5bcd007efaf6aaa5418c9b6b424ebb2770
Zubair Ahmed <zubair1024@gmail.com>
2023-01-27 16:44:21 +0400

    adding portfolio elements

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -1,18 +1,33 @@
 const portfolio = [
   {
+    img: '/portfolio/razrtrack.jpg',
+    desc: 'A web application dashboard to monitor vehicles remotely and schedule jobs',
+    title: 'Remote Vehicle Monitoring',
+  },
+  {
     img: '/portfolio/razrtrack-mobile.jpg',
     desc: 'A mobile application for remotely tracking telemetry data for vehicle and drivers',
-    title: 'RAZRTRACK Mobile',
+    title: 'Vehicle Tracking Mobile App',
   },
   {
     img: '/portfolio/razrlibre.jpg',
     desc: 'A remote health monitoring application for covid patients',
-    title: 'RAZRLIBRE Mobile',
+    title: 'COVID Tracking Mobile',
   },
   {
     img: '/portfolio/my247.jpg',
     desc: 'An App for the 4th Larger Boiler Service Company in the UK',
     title: 'My247',
+  },
+  {
+    img: '/portfolio/razrboiler.jpg',
+    desc: 'A mobile application to remotely control your thermostat and boiler from anywhere',
+    title: 'Smart Boiler Mobile',
+  },
+  {
+    img: '/portfolio/he.jpg',
+    desc: 'A web application dashboard to monitor heavy equipment for construction industry',
+    title: 'Heavy Equipment Monitoring',
   },
   {
     img: '/portfolio/tis.jpg',
