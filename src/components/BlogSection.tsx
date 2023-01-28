f8af66692aaa3355f2fde533f4f06bb5b875e0f3
Zubair Ahmed <zubair1024@gmail.com>
2023-01-29 03:14:21 +0400

    adding new blog section button

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -1,5 +1,6 @@
 import { PostCard } from '@/pages/blog';
 import { PostData } from '@/util/types';
+import Link from 'next/link';
 
 type Props = {
   allPostsData: PostData[];
@@ -15,6 +16,11 @@ const BlogSection = ({ allPostsData }: Props) => {
         {allPostsData.map((item) => {
           return <PostCard key={item.slug} data={item} />;
         })}
+      </div>
+      <div className="flex items-center justify-center py-10">
+        <Link href={'/blog'}>
+          <button className="btn btn-primary">See More Posts</button>
+        </Link>
       </div>
     </>
   );
