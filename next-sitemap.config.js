d07638768c76747e97c7879cd9fe6b1f05c0e202
Zubair Ahmed <zubair1024@gmail.com>
2023-01-29 12:02:43 +0400

    adding sitemap size

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.grizzlybit.dev',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
 };
