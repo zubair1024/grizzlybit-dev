5c9d2746e34b2d170c58f0c7c3c8f3dd2b79a012
Zubair Ahmed <zubair1024@gmail.com>
2023-01-29 11:57:56 +0400

    adding site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://example.com',
+  siteUrl: process.env.SITE_URL || 'https://www.grizzlybit.dev',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
