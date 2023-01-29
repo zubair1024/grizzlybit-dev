5df66b0cdc8d4eca07aedcb452006d3c841a8a68
Zubair Ahmed <zubair1024@gmail.com>
2023-01-29 11:40:36 +0400

    better blog section responsiveness

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -12,7 +12,7 @@ const BlogSection = ({ allPostsData }: Props) => {
       <h2 className="my-10 font-mono text-2xl font-thin tracking-wider text-center capitalize">
         Blog
       </h2>
-      <div className="grid grid-cols-2 gap-2 py-10 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-2 py-10 md:grid-cols-2 lg:grid-cols-4">
         {allPostsData.map((item) => {
           return <PostCard key={item.slug} data={item} />;
         })}
