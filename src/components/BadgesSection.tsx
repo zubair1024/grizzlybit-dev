332d0263bab3fec5f7af28f48ce1d563f283dcf4
Zubair Ahmed <zubair1024@gmail.com>
2023-01-29 11:39:08 +0400

    cleaning up deps

--- src/components/BadgesSection.tsx
+++ src/components/BadgesSection.tsx
@@ -9,14 +9,14 @@ const BadgesSection = () => {
         {badges.map((item) => {
           return (
             <Link target={'_blank'} href={item.url} key={item.title}>
-              <div className="flex items-center justify-center space-x-2">
+              <div className="flex flex-col items-center justify-center space-x-2 text-center md:flex-row md:text-left">
                 <Image
                   src={item.imageUrl}
                   height={50}
                   width={50}
                   alt={item.title}
                 ></Image>
-                <div className="text-[12px] tracking-tight leading-tight">
+                <div className="text-[12px] py-2 tracking-tight leading-tight">
                   {item.title}
                 </div>
               </div>
