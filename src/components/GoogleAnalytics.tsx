66035debab2cba44ea1e389afb5bd7938a64abd2
Zubair Ahmed <zubair1024@gmail.com>
2023-02-17 11:41:27 +0100

    better ga tracking code

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -5,7 +5,7 @@ const GoogleAnalytics = () => {
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?  id=${defaultTags.GA_TRACKING_CODE}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${defaultTags.GA_TRACKING_CODE}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -13,7 +13,8 @@ const GoogleAnalytics = () => {
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${defaultTags.GA_TRACKING_CODE}');
+
+          gtag('config', ${defaultTags.GA_TRACKING_CODE});
         `}
       </Script>
     </>
