ae2e9460de73c79ab8fefae37295049530dd8e2d
Zubair Ahmed <zubair1024@gmail.com>
2023-02-17 12:05:19 +0100

    adding gtag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -5,22 +5,18 @@ const GoogleAnalytics = () => {
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-      />
+        src="https://www.googletagmanager.com/gtag/js?id=G-9GL68GYWYP"
+      ></Script>
       <Script
         id="ga-tracking"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-        });
-        `,
+          __html: ` window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-9GL68GYWYP');`,
         }}
-      />
+      ></Script>
     </>
   );
 };
