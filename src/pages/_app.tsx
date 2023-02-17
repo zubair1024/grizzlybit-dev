7187867a08e949e25e959ea86d6f445e2c1617aa
Zubair Ahmed <zubair1024@gmail.com>
2023-02-17 20:42:25 +0100

    adding hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '@/styles/globals.css';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
+import { hotjar } from 'react-hotjar';
 import { init } from '../util/ga';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     init('G-ZD3BELKQE2');
+    hotjar.initialize(3372547, 6);
   }, []);
 
   return (
